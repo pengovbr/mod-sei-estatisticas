bca99ca306568391b3496ecf319d7385b36ba639
Starlone Passos <starlone.passos@gmail.com>
2018-04-17 21:57:58 -0300

    Implementando envio dos indicadores para o webservice

--- rn/MdEstatisticasEnviarRN.php
+++ rn/MdEstatisticasEnviarRN.php
@@ -19,6 +19,16 @@ class MdEstatisticasEnviarRN extends InfraRN {
       $json = json_encode($indicadores);
       InfraDebug::getInstance()->gravar('JSON: ' . $json, InfraLog::$INFORMACAO);
 
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, "http://192.168.1.7:8181/estatisticas");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $indicadores);
+      $output = curl_exec($ch);
+
+      InfraDebug::getInstance()->gravar('Output: ' . $output, InfraLog::$INFORMACAO);
+      curl_close($ch);
+
       return true;
 
     } catch(Exception $e) {
