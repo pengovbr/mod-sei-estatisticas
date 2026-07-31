7d3688410028bf0d03bf0cb1381661cb9fc1db9a
Paul Richard Pereira Martins dos Anjos <paul.anjos@gestao.gov.br>
2026-07-30 21:36:36 -0300

    feat: correcao no script de criacao de agendamento

--- src/scripts/cria_agendamento.php
+++ src/scripts/cria_agendamento.php
@@ -34,18 +34,16 @@ try {
 
   InfraDebug::getInstance()->gravar('INÍCIO');
 
-  // Verifica se já existe um agendamento com o comando específico
-  $sqlVerificaAgendamento = "SELECT COUNT(*) AS total FROM infra_agendamento_tarefa WHERE comando = 'MdEstatisticasAgendamentoRN::coletarIndicadores'";
+  $sqlVerificaAgendamento = "SELECT COUNT(*) AS total FROM infra_agendamento_tarefa WHERE comando = 'MdEstatisticasAgendamentoRN :: coletarIndicadores'";
   $arrVerifica = BancoSEI::getInstance()->consultarSql($sqlVerificaAgendamento);
 
   if (isset($arrVerifica[0]['total']) && $arrVerifica[0]['total'] == 0) {
-    // Calcula o próximo id disponível para infra_agendamento_tarefa
     $sqlMaxId = "SELECT MAX(id_infra_agendamento_tarefa) AS max_id FROM infra_agendamento_tarefa";
     $arrResultado = BancoSEI::getInstance()->consultarSql($sqlMaxId);
     $proxIdAgendamentoTarefa = isset($arrResultado[0]['max_id']) ? ((int)$arrResultado[0]['max_id'] + 1) : 1;
 
     $sql = "INSERT INTO infra_agendamento_tarefa (id_infra_agendamento_tarefa, descricao, comando, sta_periodicidade_execucao, periodicidade_complemento, sin_sucesso, sin_ativo)
-    VALUES (".$proxIdAgendamentoTarefa.", 'Coleta de Indicadores', 'MdEstatisticasAgendamentoRN::coletarIndicadores', 'D', '05:00', 'N', 'S')";
+    VALUES (".$proxIdAgendamentoTarefa.", 'Coleta de Indicadores', 'MdEstatisticasAgendamentoRN :: coletarIndicadores', 'D', '05:00', 'N', 'S')";
     BancoSEI::getInstance()->executarSql($sql);
   }
 
